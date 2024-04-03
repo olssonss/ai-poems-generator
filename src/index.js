89e5300d716adbd6f4b218e36bf41d6e0a29a2fa
olssonss <146712641+olssonss@users.noreply.github.com>
2024-04-03 20:57:09 +0200

    user experience completed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function displayPoem(response) {
   new Typewriter("#poem-display", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 2,
+    delay: 50,
     cursor: "",
   });
 }
@@ -16,6 +16,10 @@ function generatePoem(event) {
     "You are an expert in poems and an excel at writing short beautiful nature poems. The poem should be 4-lines and written in basic HTML. Write the full poem in the SAME language as the word the user provides. ONLY answer with the poem. Never Include titles. The poems should always be about nature, but include the topic from the instructions. Always follow the user-istructions below.";
   let prompt = `user-instruction: Write a landscape poem about ${poemInstructions.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem-display");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generate">⏳Generating a poem about ${poemInstructions.value} for you :)</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
